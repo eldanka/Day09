a8bb8a24846956e1d5588a6f97370c9e3dc3e51b
eldanka <eldankahrovic2208@gmail.com>
2021-03-16 21:14:14 +0100

    Adding ex13

---
+++ ex13/src/index.js
@@ -0,0 +1,17 @@
+function inverseWhile() {
+    var i = 5;
+    fiveNumbers = "";
+    while(i >= 0) {
+        if (i === 0) {
+            fiveNumbers += i;
+            i--; 
+        } else {
+            fiveNumbers += i + ",";
+            i--; 
+        }
+    }
+    return fiveNumbers
+}
+
+console.log(inverseWhile());
+module.exports = inverseWhile;
