a63e4aa4a89702806e70dcc19b2e726ef7913266
eldanka <eldankahrovic2208@gmail.com>
2021-03-16 19:36:38 +0100

    Adding ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,16 @@
+function logicalOrOperator(num) {
+    if(num < 20 || num > 30) {
+        return "Out";
+    }
+    return "In";
+}
+
+logicalOrOperator(0);
+logicalOrOperator(9);
+logicalOrOperator(20);
+logicalOrOperator(23);
+logicalOrOperator(30);
+logicalOrOperator(31);
+logicalOrOperator(105);
+
+module.exports = logicalOrOperator;
