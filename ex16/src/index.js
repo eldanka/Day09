2aa5673c02b599bd852a9c987ba4c2478bc5298c
eldanka <eldankahrovic2208@gmail.com>
2021-03-16 23:08:32 +0100

    Adding ex16

---
+++ ex16/src/index.js
@@ -0,0 +1,11 @@
+function checkSign(num) {
+    return num > 0 ? "positive"
+        : num < 0 ? "negative"
+        : "zero";
+}
+
+checkSign(10);
+checkSign(-12);
+checkSign(0);
+
+module.exports = checkSign;
