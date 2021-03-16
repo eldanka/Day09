c802006f9507411468a2455ef069495544a38586
eldanka <eldankahrovic2208@gmail.com>
2021-03-16 21:55:46 +0100

    Adding ex09

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,8 +1,10 @@
 function functionElse(num) {
     if (num <= 5) {
-        return "5 or smaller";
+        result = "5 or smaller";
+        return result;
     } else {
-        return "Bigger than 5";
+        result = "Bigger than 5";
+        return result;
     }
 }
 
